7796fa6fad14e265ac6c2101e774f902b60f5084
barathan76 <barathankarthikeyan06@gmail.com>
2025-06-01 15:13:46 +0530

    updated

--- packages/user_repository/lib/src/service/user_repo_address_service.dart
+++ packages/user_repository/lib/src/service/user_repo_address_service.dart
@@ -1,3 +1,27 @@
+import 'package:user_repository/src/model/user_address.dart';
 import 'package:user_repository/src/usecase/user_repo_address.dart';
 
-class UserRepoAddressService implements UserRepoAddress {}
+class UserRepoAddressService implements UserRepoAddress {
+  @override
+  Future<void> addAddress(UserAddress userAddress) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteAddress(UserAddress userAddress) {
+    // TODO: implement deleteAddress
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<UserAddress>> getAddress() {
+    // TODO: implement getAddress
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateAddress(UserAddress userAddress) {
+    // TODO: implement updateAddress
+    throw UnimplementedError();
+  }
+}
